8753cca6716bed21aad6c994bacb3d905fc11f9c
axelgutierrezlopezdev <axelgutierrezlopezdev@gmail.com>
2024-09-01 13:31:15 -0500

    populator in Items and Student for Report

---
+++ src/main/java/com/agutierrezl/matricula_facil_api/model/EnrollmentDetail.java
@@ -0,0 +1,12 @@
+package com.agutierrezl.matricula_facil_api.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EnrollmentDetail {
+    private Course course;
+}
